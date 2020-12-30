db21e684d8e67b025fe2f89a8f007d4122f982df
Edderic Ugaddan <eugaddan@panoramaed.com>
2020-12-30 10:25:21 -0500

    write information_theory module
    - implement stochastic complexity based conditional independence test

---
+++ information_theory.py
@@ -0,0 +1,416 @@
+"""
+    Information Theory
+    ------------------
+
+    Provides Information Theory helpers.
+
+    Available functions
+    -------------------
+
+    - entropy
+    - conditional_entropy
+    - conditional_mutual_information
+    - multinomial_normalizing_sum
+    - regret
+    - sci_is_independent
+
+"""
+
+import numpy as np
+
+def entropy(data, variables=[]):
+    """
+        Computes Shannon entropy.
+
+        Parameters:
+            data : pandas.DataFrame
+                A dataframe where variables are columns.
+
+            variables: list[str]
+                A list of variable names to include in the entropy calculation.
+
+        Examples:
+            Say that X is multinomially distributed with 4 classes, and they are
+            uniformly distributed. The Shannon entropy is:
+
+            - 4 * (0.25 * np.log2(0.25)) = -1 * np.log2(0.25)
+                                         = -1 * -2
+                                         = 2
+
+            >>> from pytest import approx
+            >>> size = 10000
+            >>> multinomials = np.random.multinomial(
+            >>>         n=1,
+            >>>         pvals=[0.25,
+            >>>             0.25,
+            >>>             0.25,
+            >>>             0.25],
+            >>>         size=size)
+
+            >>> x = multinomials[:, 0] \
+            >>>         + multinomials[:, 1] * 2 \
+            >>>         + multinomials[:, 2] * 3 \
+            >>>         + multinomials[:, 3] * 4
+
+            >>> df = pd.DataFrame({'x': x})
+            >>> calc = EntropyCalculator(data=df, variables=['x'])
+
+            >>> assert calc.calculate() == approx(2, abs=0.01)
+    """
+
+    data = data.copy()
+    count_col_name = 'tmp. count'
+    data[count_col_name] = 0
+    total_count = data.shape[0]
+
+    assert len(variables) > 0
+
+    variable_counts = data.groupby(variables).count()
+    probas = variable_counts / total_count
+
+    return -(probas * np.log2(probas)).sum()[count_col_name]
+
+
+def conditional_entropy(data, conditioning_set=[], variables=[]):
+    """
+        Computes H(X | Y) = H(X,Y) - H(Y) where Y is the conditioning_set and X
+        and Y are the variables.
+
+        Parameters:
+            data : pandas.DataFrame
+                A dataframe where variables are columns.
+
+            variables: list[str]
+                A list of variable names.
+
+            conditioning_set: list[str]. Defaults to empty list.
+                A list of variable names that are being conditioned on. If the
+                conditionals is not empty, then we'll be computing conditional
+                entropy.
+
+                If conditioning_set is an empty list, this function returns the
+                entropy for the set of variables (i.e. instead of computing
+                H(X|Y), it'll return H(X), the entropy of X).
+
+        Examples:
+            Say there's a variable X and Y and they are independent. X and Y are
+            multinomial variables with 4 possible values:
+
+            >>> from pytest import approx
+            >>> size = 10000
+            >>> x = np.random.multinomial(
+            >>>         n=1,
+            >>>         pvals=[0.25,
+            >>>             0.25,
+            >>>             0.25,
+            >>>             0.25],
+            >>>         size=size)
+            >>>
+            >>> x = x[:, 0] \
+            >>>         + x[:, 1] * 2 \
+            >>>         + x[:, 2] * 3 \
+            >>>         + x[:, 3] * 4
+            >>>
+            >>> y = np.random.multinomial(
+            >>>         n=1,
+            >>>         pvals=[0.25,
+            >>>             0.25,
+            >>>             0.25,
+            >>>             0.25],
+            >>>         size=size)
+            >>>
+            >>> y = y[:, 0] \
+            >>>         + y[:, 1] * 2 \
+            >>>         + y[:, 2] * 3 \
+            >>>         + y[:, 3] * 4
+            >>>
+            >>> df_2_multinomial_indep_RVs = pd.DataFrame({'x': x, 'y': y})
+
+
+            # Entropy of X, without conditioning on anything, is 2. Conditional
+            # entropy of X given Y is still 2. In other words, knowing about Y
+            # doesn't change the entropy (i.e. the uncertainty) on X.
+            # Therefore X and Y are independent.
+
+            >>> assert conditional_entropy(
+            >>>     data=df_2_multinomial_indep_RVs,
+            >>>     variables=['x', 'y'],
+            >>>     conditioning_set=['y']
+            >>> ) == approx(2, abs=0.01)
+    """
+    assert len(set(variables)) > 0
+
+    if len(conditioning_set) == 0:
+        return entropy(data=data, variables=variables)
+
+    vars_and_conditioning_set = \
+        list(set(variables).union(set(conditioning_set)))
+
+    return entropy(
+               data=data,
+               variables=vars_and_conditioning_set
+           ) - entropy(
+               data=data,
+               variables=conditioning_set
+           )
+
+def conditional_mutual_information(data, vars_1, vars_2, conditioning_set=[]):
+    """
+        Computes I(X;Y|Z) = H(X|Z) - H(X|Y,Z). Essentially, this tells us
+        whether or not Y tells us something about X, after we've known about
+        Z. In the large sample limit, if Y is independent of X given Z, then
+        conditional mutual information I(X;Y|Z) is 0. However, with finite
+        samples, even if in the true generating process, X and Y are
+        independent given Z, it's very possible that the conditional mutual
+        information is greater than 0.
+
+        Parameters:
+            data: pandas.DataFrame
+            vars_1: list[str]
+                Represents X in I(X;Y|Z).
+            vars_2: list[str]
+                Represents Y in I(X;Y|Z).
+            conditioning_set: list[str]. Defaults to empty list.
+                Represents Z in I(X;Y|Z).
+
+                If conditioning_set is empty, this computes mutual information:
+                I(X;Y) = H(X) - H(X|Y).
+
+        Examples:
+            Ex 1: Say there's a variable X and Y and they are independent. X
+            and Y are multinomial variables with 4 possible values:
+
+            >>> from pytest import approx
+            >>> size = 10000
+            >>> x = np.random.multinomial(
+            >>>         n=1,
+            >>>         pvals=[0.25,
+            >>>             0.25,
+            >>>             0.25,
+            >>>             0.25],
+            >>>         size=size)
+            >>>
+            >>> x = x[:, 0] \
+            >>>         + x[:, 1] * 2 \
+            >>>         + x[:, 2] * 3 \
+            >>>         + x[:, 3] * 4
+            >>>
+            >>> y = np.random.multinomial(
+            >>>         n=1,
+            >>>         pvals=[0.25,
+            >>>             0.25,
+            >>>             0.25,
+            >>>             0.25],
+            >>>         size=size)
+            >>>
+            >>> y = y[:, 0] \
+            >>>         + y[:, 1] * 2 \
+            >>>         + y[:, 2] * 3 \
+            >>>         + y[:, 3] * 4
+            >>>
+            >>> df_2_multinomial_indep_RVs = pd.DataFrame({'x': x, 'y': y})
+            >>>
+            >>> assert conditional_mutual_entropy(
+            >>>     data=df_2_multinomial_indep_RVs,
+            >>>     variables=['x', 'y'],
+            >>>     conditioning_set=[]
+            >>> ) == approx(0, abs=0.01)
+
+            Ex 2: Z causes X and Y:
+
+            >>> from pytest import approx
+            >>> size = 10000
+            >>> z = np.random.multinomial(
+            >>>         n=1,
+            >>>         pvals=[0.25,
+            >>>             0.25,
+            >>>             0.25,
+            >>>             0.25],
+            >>>         size=size)
+            >>>
+            >>> z = z[:, 0] \
+            >>>         + z[:, 1] * 2 \
+            >>>         + z[:, 2] * 3 \
+            >>>         + z[:, 3] * 4
+            >>> y = (z == 2)
+            >>> x = (z == 1)
+            >>>
+            >>> df_2_multinomial_indep_RVs = pd.DataFrame(
+            >>>     {'x': x, 'y': y, 'z': z}
+            >>> )
+            >>>
+            >>> assert conditional_mutual_entropy(
+            >>>     data=df_2_multinomial_indep_RVs,
+            >>>     variables=['x', 'y'],
+            >>>     conditioning_set=['z']
+            >>> ) == approx(0, abs=0.01)
+    """
+    return conditional_entropy(
+        data=data,
+        variables=vars_1,
+        conditioning_set=conditioning_set
+    ) - conditional_entropy(
+        data=data,
+        variables=vars_1,
+        conditioning_set=list(set(conditioning_set).union(vars_2))
+    )
+
+
+def multinomial_normalizing_sum(num_classes, sample_size):
+    """
+        Implementation of a fast algorithm for computing the multinomial
+        normalizing sum. This is used in the Normalized Maximum Likelihood
+        (NML) distribution, which can be used for measuring Stochastic
+        Complexity. The latter is a model selection technique and can be used
+        for conditional independence testing.
+
+        See "Computing the Multinomial Stochastic Complexity in Sub-Linear
+        Time" by Mononen and Myllmäki.
+
+        Parameters:
+            num_classes: int
+                Number of classes
+            sample_size: int
+                Sample size
+    """
+    d = 10
+    b = 1
+    summation = 1
+
+    bound = int(np.ceil(2 + np.sqrt(-2 * sample_size * np.log2(2 * 10**(-d) - 100**(-d)))))
+
+    for k in range(1, bound):
+        b = (num_classes - k + 1) / sample_size * b
+        summation = summation + b
+
+    old_sum = 1
+
+    for j in range(3,num_classes):
+        new_sum = summation + (sample_size * old_sum) / (j-2)
+        old_sum = summation
+        summation = new_sum
+
+    return summation
+
+def regret(data, variables=[], conditioning_set=[]):
+    assert len(variables) > 0
+
+    data_copy = data.copy()
+    data_copy['tmp counts'] = 0
+
+    counts = data_copy.groupby(variables).count()
+
+    if len(conditioning_set) == 0:
+        return np.log2(
+            multinomial_normalizing_sum(
+                num_classes=counts.shape[0],
+                sample_size=data.shape[0]
+            )
+        )
+
+    conditioning_set_counts = \
+        data_copy.\
+        groupby(conditioning_set).\
+        count()[['tmp counts']]
+
+    summation = 0
+
+    for indices, count in conditioning_set_counts.iterrows():
+        summation += np.log2(
+            multinomial_normalizing_sum(
+                num_classes=counts.shape[0],
+                sample_size=count
+            )
+        )
+
+    return summation['tmp counts']
+
+def sci_is_independent(data, vars_1=[], vars_2=[], conditioning_set=[]):
+    """
+        This is an implementation of Stochastic Complexity Based
+        Conditional Independence Based Test.
+
+        Conditional Independence Testing, which answers the question "Is vars_1
+        independent of vars_2 given the conditioning set?" is very important in
+        causality, causal discovery, and statistics. One metric that measures
+        this is the conditional mutual information (CMI) statistic. In the
+        large sample limit, if the CMI is 0, then vars_1 and vars_2 are
+        independent given the conditioning set.  However, CMI is usually never
+        0 when sample sizes are small, even if the data was sampled from
+        variables that are independent from each other. People usually select a
+        threshold to decide whether the given set of variables are independent
+        from each other. The threshold should depend on sample size and the
+        number of possible values (i.e. the domain) for each variable.
+        Stochastic Complexity Based Conditional Independence Based Test helps
+        us automatically select a threshold for deciding which value of
+        conditional mutual information is small enough to consider
+        "independent" given sample size and complexity of the distributions.
+
+        Parameters:
+            data: pandas.DataFrame
+
+            vars_1: list[str]
+                A set of variables present in data.  Disjoint from vars_2 and
+                conditioning_set.
+
+            vars_2: list[str]
+                Another set of variables present in data. Disjoint from vars_1
+                and conditioning_set.
+
+            conditioning_set: list[str]. Defaults to empty list.
+                Disjoint from vars_1 and vars_2.
+
+        Returns true if vars_1 is independent from vars_2 given conditioning
+        set, false otherwise.
+
+        Examples:
+            Ex 1: Z causes X and Y:
+
+            >>> from pytest import approx
+            >>> size = 10000
+            >>> z = np.random.multinomial(
+            >>>         n=1,
+            >>>         pvals=[0.25,
+            >>>             0.25,
+            >>>             0.25,
+            >>>             0.25],
+            >>>         size=size)
+            >>>
+            >>> z = z[:, 0] \
+            >>>         + z[:, 1] * 2 \
+            >>>         + z[:, 2] * 3 \
+            >>>         + z[:, 3] * 4
+            >>> y = (z == 2)
+            >>> x = (z == 1)
+            >>>
+            >>> df_2_multinomial_indep_RVs = pd.DataFrame(
+            >>>     {'x': x, 'y': y, 'z': z}
+            >>> )
+            >>>
+            >>> assert sci_is_independent(
+            >>>     data=df_2_multinomial_indep_RVs,
+            >>>     variables=['x', 'y'],
+            >>>     conditioning_set=['z']
+            >>> ) == True
+    """
+    sample_size = data.shape[0]
+
+    stochastic_complexity_score = sample_size \
+        * conditional_mutual_information(
+            data=data,
+            vars_1=vars_1,
+            vars_2=vars_2,
+            conditioning_set=conditioning_set
+        ) \
+        + regret(
+            data=data,
+            variables=vars_1,
+            conditioning_set=conditioning_set
+          )  \
+        - regret(
+            data=data,
+            variables=vars_1,
+            conditioning_set=list(set(conditioning_set).union(vars_2))
+        )
+
+    return stochastic_complexity_score <= 0
---
+++ information_theory_test.py
@@ -0,0 +1,268 @@
+import pandas as pd
+import numpy as np
+import pytest
+from pytest import approx
+from information_theory import entropy, conditional_entropy, multinomial_normalizing_sum, conditional_mutual_information, sci_is_independent
+
+@pytest.fixture
+def multinomial_RV():
+    def _setup(size=10000):
+        multinomial = np.random.multinomial(
+                n=1,
+                pvals=[0.25,
+                    0.25,
+                    0.25,
+                    0.25],
+                size=size)
+
+        return multinomial[:, 0] \
+                + multinomial[:, 1] * 2 \
+                + multinomial[:, 2] * 3 \
+                + multinomial[:, 3] * 4
+
+    yield _setup
+
+@pytest.fixture
+def df_2_multinomial_indep_RVs(multinomial_RV):
+    def _setup(size=10000):
+        x = multinomial_RV(size=size)
+        y = multinomial_RV(size=size)
+
+        return pd.DataFrame({'x': x, 'y': y})
+
+    yield _setup
+
+@pytest.fixture
+def df_X_and_Y_are_deterministic(multinomial_RV):
+    def _setup(size=10000):
+        x = multinomial_RV(size=size)
+        return pd.DataFrame({'x': x, 'y': x})
+    yield _setup
+
+@pytest.fixture
+def df_X_Y_Z_are_deterministic(multinomial_RV):
+    def _setup(size=10000):
+        x = multinomial_RV(size=size)
+        return pd.DataFrame({'x': x, 'y': x, 'z': x})
+    yield _setup
+
+@pytest.fixture
+def df_X_causes_Y(multinomial_RV):
+    def _setup(size=10000):
+        x = multinomial_RV(size=size)
+        y = (x == 2)
+
+        return pd.DataFrame({'x': x, 'y': y})
+    yield _setup
+
+@pytest.fixture
+def df_Z_causes_X_and_Y(multinomial_RV):
+    def _setup(size=10000):
+        z = multinomial_RV(size=size)
+
+        y = (z == 2)
+        x = (z == 1)
+
+        return pd.DataFrame({'x': x, 'y': y, 'z': z})
+    yield _setup
+
+@pytest.fixture
+def df_X_and_Y_cause_Z(multinomial_RV):
+    def _setup(size=10000, proba_noise=0.1):
+        x = multinomial_RV(size=size)
+        y = multinomial_RV(size=size)
+        noise = np.random.binomial(n=1, p=1-proba_noise, size=size)
+
+        z = (x == 1) & (y == 2) & noise
+
+        return pd.DataFrame({'x': x, 'y': y, 'z': z})
+    yield _setup
+
+def test_entropy_uniform_multinomial_with_4_possible_values_size_10000():
+    size = 10000
+    multinomials = np.random.multinomial(
+            n=1,
+            pvals=[0.25,
+                0.25,
+                0.25,
+                0.25],
+            size=size)
+
+    x = multinomials[:, 0] \
+            + multinomials[:, 1] * 2 \
+            + multinomials[:, 2] * 3 \
+            + multinomials[:, 3] * 4
+
+    df = pd.DataFrame({'x': x})
+
+    assert entropy(data=df, variables=['x']) == approx(2, abs=0.01)
+
+def test_cond_ent_uniform_multinomial_with_4_possible_values(
+    df_2_multinomial_indep_RVs
+):
+    assert conditional_entropy(
+               data=df_2_multinomial_indep_RVs(size=10000),
+               variables=['x', 'y'],
+               conditioning_set=['y']
+           ) == approx(2, abs=0.01)
+
+def test_cond_mut_inf_uniform_multinomial_with_4_possible_values_size_10000(
+    df_2_multinomial_indep_RVs
+):
+    assert conditional_mutual_information(
+               data=df_2_multinomial_indep_RVs(),
+               vars_1=['x'],
+               vars_2=['y'],
+               conditioning_set=[]
+           ) == approx(0, abs=0.01)
+
+def test_sci_indep_uniform_multinomial_with_4_possible_values_size_10000(
+    df_2_multinomial_indep_RVs
+):
+    params = {
+       "data": df_2_multinomial_indep_RVs(size=10000),
+       "vars_1": ['x'],
+       "vars_2": ['y'],
+       "conditioning_set": []
+    }
+
+    params["data"] = df_2_multinomial_indep_RVs(size=10000)
+    assert sci_is_independent(**params) == True
+
+    params["data"] = df_2_multinomial_indep_RVs(size=1000)
+    assert sci_is_independent(**params) == True
+
+    params["data"] = df_2_multinomial_indep_RVs(size=100)
+    assert sci_is_independent(**params) == True
+
+def test_sci_indep_X_Y_Z_are_deterministic(
+    df_X_Y_Z_are_deterministic
+):
+    params = {
+       "data": df_X_Y_Z_are_deterministic(size=100),
+       "vars_1": ['x'],
+       "vars_2": ['y'],
+       "conditioning_set": ['z']
+    }
+
+    assert sci_is_independent(**params) == True
+
+def test_sci_indep_X_and_Y_are_deterministic(
+    df_X_and_Y_are_deterministic
+):
+    params = {
+       "data": df_X_and_Y_are_deterministic(size=100),
+       "vars_1": ['x'],
+       "vars_2": ['y'],
+       "conditioning_set": []
+    }
+
+    assert sci_is_independent(**params) == False
+
+def test_sci_indep_X_causes_Y(
+    df_X_causes_Y
+):
+    params = {
+       "data": df_X_causes_Y(size=100),
+       "vars_1": ['x'],
+       "vars_2": ['y'],
+       "conditioning_set": []
+    }
+
+    assert sci_is_independent(**params) == False
+
+def test_cmi_indep_X_causes_Y(
+    df_X_causes_Y
+):
+    params = {
+       "data": df_X_causes_Y(size=100),
+       "vars_1": ['x'],
+       "vars_2": ['y'],
+       "conditioning_set": []
+    }
+
+    assert conditional_mutual_information(**params) \
+        != approx(0, abs=0.01)
+
+def test_cmi_indep_Z_causes_X_and_Y(
+    df_Z_causes_X_and_Y
+):
+    params = {
+       "data": df_Z_causes_X_and_Y(size=500),
+       "vars_1": ['x'],
+       "vars_2": ['y'],
+       "conditioning_set": ['z']
+    }
+
+    assert conditional_mutual_information(**params) \
+        == approx(0, abs=0.01)
+
+def test_sci_indep_Z_causes_X_and_Y(
+    df_Z_causes_X_and_Y
+):
+    params_1 = {
+       "data": df_Z_causes_X_and_Y(size=500),
+       "vars_1": ['x'],
+       "vars_2": ['y'],
+       "conditioning_set": ['z']
+    }
+
+    assert sci_is_independent(**params_1) == True
+
+    params_2 = {
+       "data": df_Z_causes_X_and_Y(size=500),
+       "vars_1": ['x'],
+       "vars_2": ['y'],
+       "conditioning_set": []
+    }
+
+    assert sci_is_independent(**params_2) == False
+
+def test_sci_indep_X_and_Y_cause_Z(
+    df_X_and_Y_cause_Z
+):
+    params_1 = {
+       "data": df_X_and_Y_cause_Z(size=500),
+       "vars_1": ['x'],
+       "vars_2": ['y'],
+       "conditioning_set": []
+    }
+
+    assert sci_is_independent(**params_1) == True
+
+    params_2 = {
+       "data": df_X_and_Y_cause_Z(size=500),
+       "vars_1": ['x'],
+       "vars_2": ['y'],
+       "conditioning_set": ['z']
+    }
+
+    assert sci_is_independent(**params_2) == False
+
+def test_sci_indep_X_Y_Z_indep(
+    multinomial_RV
+):
+    size=1000
+    df = pd.DataFrame(
+        {
+            'x': multinomial_RV(size=size),
+            'y': multinomial_RV(size=size),
+            'z': multinomial_RV(size=size),
+        }
+    )
+
+    params = {
+       "data": df,
+       "vars_1": ['x'],
+       "vars_2": ['y'],
+       "conditioning_set": ['z']
+    }
+
+    assert sci_is_independent(**params) == True
+
+def test_multinomial_normalizing_sum():
+    mns = multinomial_normalizing_sum(num_classes=4, sample_size=5)
+
+    assert mns == approx(7.51, abs=0.01)
+
+
