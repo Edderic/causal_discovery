be78c4252864c405a25a8cfa8c3dcda040e1d66a
Edderic Ugaddan <eugaddan@panoramaed.com>
2021-01-07 22:27:07 -0500

    add fixtures

--- conftest.py
+++ conftest.py
@@ -77,6 +77,27 @@ def df_X_and_Y_cause_Z(multinomial_RV):
     yield _setup
 
 @pytest.fixture
+def df_X_and_Y_cause_Z_and_Z_cause_MI_X(df_X_and_Y_cause_Z):
+    def _setup(size=10000, proba_noise=0.1):
+        df = df_X_and_Y_cause_Z(size=size, proba_noise=proba_noise)
+
+        missingness_of_x = \
+            np.random.binomial(n=1, p=proba_noise, size=size) \
+            & df['z'].isin([1])
+
+        missingness_indices = np.where(missingness_of_x == 1)
+
+        df.at[missingness_indices[0], 'x'] = np.nan
+
+        return df.merge(
+            df.isnull().add_prefix('MI_'),
+            left_index=True,
+            right_index=True
+        )
+
+    yield _setup
+
+@pytest.fixture
 def df_X_Y_indep_Y_causes_MI_X(df_2_multinomial_indep_RVs):
     def _setup(size=10000, proba_noise=0.1):
 
@@ -108,6 +129,46 @@ def df_Z_causes_X_Y_and_X_Z_causes_MI_Y(df_Z_causes_X_and_Y):
         missingness_indices = np.where(missingness_of_y == 1)
 
         df.at[missingness_indices[0], 'y'] = np.nan
+
+        return df
+
+    yield _setup
+
+@pytest.fixture
+def df_long_chains_and_collider_without_MI(multinomial_RV):
+    def _setup(size=10000, proba_noise=0.1):
+        a = multinomial_RV(size=size)
+        e = multinomial_RV(size=size)
+
+        b = (a == 1) & np.random.binomial(n=1, p=1-proba_noise, size=size)
+        d = (e == 1) & np.random.binomial(n=1, p=1-proba_noise, size=size)
+
+        # collider
+        c = b & d & np.random.binomial(n=1, p=1-proba_noise, size=size)
+
+        df = pd.DataFrame(
+            {
+                'a': a,
+                'b': b,
+                'c': c,
+                'd': d,
+                'e': e,
+            }
+        )
+
+        return df
+
+    yield _setup
+
+@pytest.fixture
+def df_long_chains_and_collider_with_MI(df_long_chains_and_collider_without_MI):
+    def _setup(size=10000, proba_noise=0.1):
+        df = df_long_chains_and_collider_without_MI(size=size, proba_noise=proba_noise)
+        MI_a = df['c'] * np.random.binomial(n=1, p=1-proba_noise, size=size)
+
+        missingness_indices = np.where(MI_a == 1)
+
+        df.at[missingness_indices[0], 'a'] = np.nan
 
         return df
 
