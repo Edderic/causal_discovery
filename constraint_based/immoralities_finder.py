178c69851723d57d369000bdf480f27daeeea5e5
Edderic Ugaddan <eugaddan@panoramaed.com>
2021-01-11 10:15:43 -0500

    implemented & tested immoralities finder

--- constraint_based/immoralities_finder.py
+++ constraint_based/immoralities_finder.py
@@ -1,9 +1,70 @@
+from ..graphs.marked_pattern_graph import get_nodes_from_edges
+from ..graphs.marked_pattern_graph import get_nodes_adj_to_node
+from .misc import key_for_pair
+
 class ImmoralitiesFinder(object):
     """
+        Finds immoralities. An immorality consists of two edges made up of
+        three nodes. Two nodes are non-adjacent, and the third node is adjacent
+        with the other two nodes.
+
+        Parameters:
+            marked_pattern_graph: graphs.MarkedPatternGraph
     """
-    def __init__(self, marked_pattern_graph):
+    def __init__(self, marked_pattern_graph, cond_sets_satisfying_cond_indep):
         self.marked_pattern_graph = marked_pattern_graph
-        pass
+        self.cond_sets_satisfying_cond_indep = cond_sets_satisfying_cond_indep
 
     def find(self):
-        pass
+        undirected_nodes = \
+            get_nodes_from_edges(self.marked_pattern_graph.undirected_edges)
+
+        unmarked_arrows = set({})
+
+        for node_1 in undirected_nodes:
+            for node_2 in undirected_nodes:
+                if node_1 == node_2:
+                    continue
+
+                nodes_adj_to_node_1 = get_nodes_adj_to_node(
+                    edges=self.marked_pattern_graph.undirected_edges,
+                    node=node_1
+                )
+
+                nodes_adj_to_node_2 = get_nodes_adj_to_node(
+                    edges=self.marked_pattern_graph.undirected_edges,
+                    node=node_2,
+                )
+
+                # if node_1 and node_2 are adjacent, continue.
+                if set({node_1, node_2}).intersection(nodes_adj_to_node_1.union(nodes_adj_to_node_2)) != set({}):
+                    continue
+
+                common_adjacent_nodes = \
+                    nodes_adj_to_node_1\
+                    .intersection(nodes_adj_to_node_2)
+
+                _common_adjacent_nodes = list(common_adjacent_nodes)
+
+                for common_adj_node in _common_adjacent_nodes:
+                    try:
+                        if self.\
+                                _get_cond_set_vars_for_pair(
+                                    pair=[node_1, node_2]
+                                ).intersection(set({common_adj_node})) == set({}):
+                            unmarked_arrows = unmarked_arrows.union(set({(node_1, common_adj_node)}))
+                            unmarked_arrows = unmarked_arrows.union(set({(node_2, common_adj_node)}))
+
+                    except KeyError:
+                        continue
+
+        return unmarked_arrows
+
+    def _get_cond_set_vars_for_pair(self, pair):
+        cond_sets = self.cond_sets_satisfying_cond_indep[
+            key_for_pair(pair)
+        ]
+
+        return get_nodes_from_edges(cond_sets)
+
+
--- constraint_based/immoralities_finder_test.py
+++ constraint_based/immoralities_finder_test.py
@@ -1,5 +1,53 @@
 import pytest
 from ..graphs.marked_pattern_graph import MarkedPatternGraph
+from .immoralities_finder import ImmoralitiesFinder
+
+def test_simple_chain():
+    graph = MarkedPatternGraph(
+        nodes=['X', 'Y', 'Z'],
+        undirected_edges=[
+            set({'X', 'Y'}),
+            set({'Y', 'Z'})
+        ]
+    )
+
+    cond_sets_satisfying_cond_indep = {
+        'X _||_ Z': [ set({'Y'}) ],
+        'X _||_ Y': [],
+        'Y _||_ Z': [],
+    }
+
+    unmarked_arrows = ImmoralitiesFinder(
+        marked_pattern_graph=graph,
+        cond_sets_satisfying_cond_indep=cond_sets_satisfying_cond_indep
+    ).find()
+
+    assert set(unmarked_arrows) == set({})
+
+def test_simple_collider():
+    graph = MarkedPatternGraph(
+        nodes=['Parent 1', 'Parent 2', 'collider'],
+        undirected_edges=[
+            set({'Parent 1', 'collider'}),
+            set({'Parent 2', 'collider'})
+        ]
+    )
+
+    cond_sets_satisfying_cond_indep = {
+        'Parent 1 _||_ Parent 2': [ set({}) ],
+    }
+
+    unmarked_arrows = ImmoralitiesFinder(
+        marked_pattern_graph=graph,
+        cond_sets_satisfying_cond_indep=cond_sets_satisfying_cond_indep
+    ).find()
+
+    assert set(unmarked_arrows) == set(
+        {
+            ('Parent 1', 'collider'),
+            ('Parent 2', 'collider')
+        }
+    )
 
 def test_firing_squad_example():
     graph = MarkedPatternGraph(
@@ -7,19 +55,37 @@ def test_firing_squad_example():
             'Captain ordered to shoot',
             'Rifleman 1 shot',
             'Rifleman 2 shot',
-            'Prisoner shot',
+            'Prisoner hit by bullet',
             'Prisoner dead'
         ],
         undirected_edges=[
+            set({'Rifleman 1 shot', 'Prisoner hit by bullet'}),
+            set({'Rifleman 2 shot', 'Prisoner hit by bullet'}),
             set({'Captain ordered to shoot', 'Rifleman 1 shot'}),
             set({'Captain ordered to shoot', 'Rifleman 2 shot'}),
-            set({'Prisoner shot', 'Prisoner dead'})
-        ],
-        unmarked_arrows=[
-            ('Rifleman 1 shot', 'Prisoner shot'),
-            ('Rifleman 2 shot', 'Prisoner shot')
+            set({'Prisoner hit by bullet', 'Prisoner dead'})
         ]
     )
 
-    IgT
+    cond_sets_satisfying_cond_indep = {
+        'Captain ordered to shoot _||_ Prisoner hit by bullet': [
+            set({'Rifleman 1 shot'}),
+            set({'Rifleman 2 shot'}),
+            set({'Rifleman 1 shot', 'Rifleman 2 shot'})
+        ],
+        'Rifleman 1 shot _||_ Rifleman 2 shot': [set({'Captain ordered to shoot'})],
+        'Prisoner dead _||_ Rifleman 1 shot': [set({'Prisoner hit by bullet'})],
+        'Prisoner dead _||_ Rifleman 2 shot': [set({'Prisoner hit by bullet'})]
+    }
 
+    unmarked_arrows = ImmoralitiesFinder(
+        marked_pattern_graph=graph,
+        cond_sets_satisfying_cond_indep=cond_sets_satisfying_cond_indep
+    ).find()
+
+    assert set(unmarked_arrows) == set(
+        {
+            ('Rifleman 1 shot', 'Prisoner hit by bullet'),
+            ('Rifleman 2 shot', 'Prisoner hit by bullet')
+        }
+    )
