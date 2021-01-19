use correct multinomial normalizing sum; extract sci_is_independent test

conftest.py
@@ -4,14 +4,18 @@ import pandas as pd
 
 @pytest.fixture
 def multinomial_RV():
-    def _setup(size=10000):
+    def _setup(
+        pvals=[0.25,
+            0.25,
+            0.25,
+            0.25
+        ],
+        size=10000
+    ):
         multinomial = np.random.multinomial(
-                n=1,
-                pvals=[0.25,
-                    0.25,
-                    0.25,
-                    0.25],
-                size=size)
+            n=1,
+            pvals=pvals,
+            size=size)
 
         return multinomial[:, 0] \
                 + multinomial[:, 1] * 2 \
@@ -67,7 +71,7 @@ def df_Z_causes_X_and_Y(multinomial_RV):
 @pytest.fixture
 def df_X_and_Y_cause_Z(multinomial_RV):
     def _setup(size=10000, proba_noise=0.1):
-        x = multinomial_RV(size=size)
+        x = multinomial_RV(size=size, pvals=[0.2,0.3,0.4,0.1])
         y = multinomial_RV(size=size)
         noise = np.random.binomial(n=1, p=1-proba_noise, size=size)
 
@@ -89,11 +93,7 @@ def df_X_and_Y_cause_Z_and_Z_cause_MI_X(df_X_and_Y_cause_Z):
 
         df.at[missingness_indices[0], 'x'] = np.nan
 
-        return df.merge(
-            df.isnull().add_prefix('MI_'),
-            left_index=True,
-            right_index=True
-        )
+        return df
 
     yield _setup
 
@@ -164,7 +164,7 @@ def df_long_chains_and_collider_without_MI(multinomial_RV):
 def df_long_chains_and_collider_with_MI(df_long_chains_and_collider_without_MI):
     def _setup(size=10000, proba_noise=0.1):
         df = df_long_chains_and_collider_without_MI(size=size, proba_noise=proba_noise)
-        MI_a = df['c'] * np.random.binomial(n=1, p=1-proba_noise, size=size)
+        MI_a = df['c'] * np.random.binomial(n=1, p=proba_noise, size=size)
 
         missingness_indices = np.where(MI_a == 1)
 

information_theory.py
@@ -18,7 +18,7 @@
 
 import numpy as np
 
-def entropy(data, variables=[]):
+def entropy(data, variables=[], base_2=True):
     """
         Computes Shannon entropy.
 
@@ -68,7 +68,12 @@ def entropy(data, variables=[]):
     variable_counts = data.groupby(list(variables)).count()
     probas = variable_counts / total_count
 
-    return -(probas * np.log2(probas)).sum()[count_col_name]
+    if base_2:
+        log_func = np.log2
+    else:
+        log_func = np.log
+
+    return -(probas * log_func(probas)).sum()[count_col_name]
 
 
 def conditional_entropy(data, conditioning_set=[], variables=[]):
@@ -255,8 +260,7 @@ def conditional_mutual_information(data, vars_1, vars_2, conditioning_set=[]):
         conditioning_set=list(set(conditioning_set).union(vars_2))
     )
 
-
-def multinomial_normalizing_sum(num_classes, sample_size):
+def multinomial_normalizing_sum(num_classes, sample_size, d=10):
     """
         Implementation of a fast algorithm for computing the multinomial
         normalizing sum. This is used in the Normalized Maximum Likelihood
@@ -272,20 +276,21 @@ def multinomial_normalizing_sum(num_classes, sample_size):
                 Number of classes
             sample_size: int
                 Sample size
+            d: Number of digits (for precision)
+
     """
-    d = 10
     b = 1
     summation = 1
 
     bound = int(np.ceil(2 + np.sqrt(-2 * sample_size * np.log2(2 * 10**(-d) - 100**(-d)))))
 
-    for k in range(1, bound):
-        b = (num_classes - k + 1) / sample_size * b
+    for k in range(1, bound + 1):
+        b = (sample_size - k + 1) / sample_size * b
         summation = summation + b
 
     old_sum = 1
 
-    for j in range(3,num_classes):
+    for j in range(3,num_classes+1):
         new_sum = summation + (sample_size * old_sum) / (j-2)
         old_sum = summation
         summation = new_sum
@@ -298,12 +303,12 @@ def regret(data, variables=[], conditioning_set=[]):
     data_copy = data.copy()
     data_copy['tmp counts'] = 0
 
-    counts = data_copy.groupby(variables).count()
+    num_classes = data_copy.groupby(variables).count().shape[0]
 
     if len(conditioning_set) == 0:
-        return np.log2(
+        return np.log(
             multinomial_normalizing_sum(
-                num_classes=counts.shape[0],
+                num_classes=num_classes,
                 sample_size=data.shape[0]
             )
         )
@@ -316,9 +321,9 @@ def regret(data, variables=[], conditioning_set=[]):
     summation = 0
 
     for indices, count in conditioning_set_counts.iterrows():
-        summation += np.log2(
+        summation += np.log(
             multinomial_normalizing_sum(
-                num_classes=counts.shape[0],
+                num_classes=num_classes,
                 sample_size=count
             )
         )
@@ -332,20 +337,21 @@ def stochastic_complexity_score(
     conditioning_set,
     sample_size
 ):
-    return sample_size \
-        * conditional_mutual_information(
+    # TODO: add back sample size after debugging?
+    # import pdb; pdb.set_trace()
+    return sample_size * \
+        conditional_mutual_information(
             data=testwise_deleted_data,
             vars_1=vars_1,
             vars_2=vars_2,
             conditioning_set=conditioning_set
-        ) \
-        + regret(
+        ) + \
+        regret(
             data=testwise_deleted_data,
             variables=vars_1,
             conditioning_set=conditioning_set
-        ) \
-        - regret(
+        ) - \
+        regret(
             data=testwise_deleted_data,
             variables=vars_1,
-            conditioning_set=list(set(conditioning_set).union(vars_2))
-        )
+            conditioning_set=list(set(conditioning_set).union(vars_2)))

information_theory_test.py
@@ -42,61 +42,6 @@ def test_cond_mut_inf_uniform_multinomial_with_4_possible_values_size_10000(
                conditioning_set=[]
            ) == approx(0, abs=0.01)
 
-def test_sci_indep_uniform_multinomial_with_4_possible_values_size_10000(
-    df_2_multinomial_indep_RVs
-):
-    params = {
-       "data": df_2_multinomial_indep_RVs(size=10000),
-       "vars_1": ['x'],
-       "vars_2": ['y'],
-       "conditioning_set": []
-    }
-
-    params["data"] = df_2_multinomial_indep_RVs(size=10000)
-    assert sci_is_independent(**params) == True
-
-    params["data"] = df_2_multinomial_indep_RVs(size=1000)
-    assert sci_is_independent(**params) == True
-
-    params["data"] = df_2_multinomial_indep_RVs(size=100)
-    assert sci_is_independent(**params) == True
-
-def test_sci_indep_X_Y_Z_are_deterministic(
-    df_X_Y_Z_are_deterministic
-):
-    params = {
-       "data": df_X_Y_Z_are_deterministic(size=100),
-       "vars_1": ['x'],
-       "vars_2": ['y'],
-       "conditioning_set": ['z']
-    }
-
-    assert sci_is_independent(**params) == True
-
-def test_sci_indep_X_and_Y_are_deterministic(
-    df_X_and_Y_are_deterministic
-):
-    params = {
-       "data": df_X_and_Y_are_deterministic(size=100),
-       "vars_1": ['x'],
-       "vars_2": ['y'],
-       "conditioning_set": []
-    }
-
-    assert sci_is_independent(**params) == False
-
-def test_sci_indep_X_causes_Y(
-    df_X_causes_Y
-):
-    params = {
-       "data": df_X_causes_Y(size=100),
-       "vars_1": ['x'],
-       "vars_2": ['y'],
-       "conditioning_set": []
-    }
-
-    assert sci_is_independent(**params) == False
-
 def test_cmi_indep_X_causes_Y(
     df_X_causes_Y
 ):
@@ -123,72 +68,16 @@ def test_cmi_indep_Z_causes_X_and_Y(
     assert conditional_mutual_information(**params) \
         == approx(0, abs=0.01)
 
-def test_sci_indep_Z_causes_X_and_Y(
-    df_Z_causes_X_and_Y
-):
-    params_1 = {
-       "data": df_Z_causes_X_and_Y(size=500),
-       "vars_1": ['x'],
-       "vars_2": ['y'],
-       "conditioning_set": ['z']
-    }
+# Page 10 of
+# https://hekyll.services.adelaide.edu.au/dspace/bitstream/2440/23025/2/hdl_23025.pdf
+# says the normalizing constant should be 1.53 for num_classes 2 and
+# sample_size 10
+def test_binomial_example_with_10_data_points():
+    mns = multinomial_normalizing_sum(num_classes=2, sample_size=10)
 
-    assert sci_is_independent(**params_1) == True
+    assert np.log(mns) == approx(1.53, abs=0.01)
 
-    params_2 = {
-       "data": df_Z_causes_X_and_Y(size=500),
-       "vars_1": ['x'],
-       "vars_2": ['y'],
-       "conditioning_set": []
-    }
+def test_binomial_example_with_10_data_points_mns0():
+    mns = multinomial_normalizing_sum(num_classes=2, sample_size=2)
 
-    assert sci_is_independent(**params_2) == False
-
-def test_sci_indep_X_and_Y_cause_Z(
-    df_X_and_Y_cause_Z
-):
-    params_1 = {
-       "data": df_X_and_Y_cause_Z(size=500),
-       "vars_1": ['x'],
-       "vars_2": ['y'],
-       "conditioning_set": []
-    }
-
-    assert sci_is_independent(**params_1) == True
-
-    params_2 = {
-       "data": df_X_and_Y_cause_Z(size=500),
-       "vars_1": ['x'],
-       "vars_2": ['y'],
-       "conditioning_set": ['z']
-    }
-
-    assert sci_is_independent(**params_2) == False
-
-def test_sci_indep_X_Y_Z_indep(
-    multinomial_RV
-):
-    size=1000
-    df = pd.DataFrame(
-        {
-            'x': multinomial_RV(size=size),
-            'y': multinomial_RV(size=size),
-            'z': multinomial_RV(size=size),
-        }
-    )
-
-    params = {
-       "data": df,
-       "vars_1": ['x'],
-       "vars_2": ['y'],
-       "conditioning_set": ['z']
-    }
-
-    assert sci_is_independent(**params) == True
-
-def test_multinomial_normalizing_sum():
-    mns = multinomial_normalizing_sum(num_classes=4, sample_size=5)
-
-    assert mns == approx(7.51, abs=0.01)
-
-
+    assert mns == approx(2.5, abs=0.01)