9d107894f9cfe7fdcf7b688334a8dd63a5c2cf1e
Edderic Ugaddan <eugaddan@panoramaed.com>
2021-01-22 17:00:46 -0500

    add more SkeletonFinder tests

--- constraint_based/skeleton_finder_test.py
+++ constraint_based/skeleton_finder_test.py
@@ -4,7 +4,7 @@ from .skeleton_finder import SkeletonFinder
 
 def test_2_multinom_RVs(df_2_multinomial_indep_RVs):
     skeleton_finder = SkeletonFinder(
-        data=df_2_multinomial_indep_RVs(size=1000),
+        data=df_2_multinomial_indep_RVs(size=10000),
         var_names=['x', 'y'],
     )
 
@@ -38,3 +38,103 @@ def test_skeleton_finder_Z_causes_X_and_Y(df_Z_causes_X_and_Y):
     assert graph.undirected_edges == [set(('x', 'z')), set(('y', 'z'))]
     assert cond_sets_satisfying_cond_indep == {'x _||_ y': [set(('z'))]}
 
+def test_long_chains_collider_bias_without_MI(
+    df_long_chains_and_collider_without_MI
+):
+    size = 100000
+
+    df = df_long_chains_and_collider_without_MI(size=size)
+    skeleton_finder = SkeletonFinder(
+        data=df,
+        var_names=df.columns,
+        only_find_one=True
+    )
+
+    graph, cond_sets_satisfying_cond_indep = skeleton_finder.find()
+
+    expected_undirected_edges = frozenset({
+        frozenset({'a', 'b'}),
+        frozenset({'d', 'e'}),
+        frozenset({'b', 'c'}),
+        frozenset({'c', 'd'}),
+    })
+
+    assert frozenset(graph.undirected_edges) == expected_undirected_edges
+
+@pytest.mark.focus
+def test_long_chains_collider_bias_with_MI(
+    df_long_chains_and_collider_with_MI
+):
+    size = 10000
+
+    df = df_long_chains_and_collider_with_MI(size=size, proba_noise=0.7)
+    skeleton_finder = SkeletonFinder(
+        data=df,
+        var_names=df.columns,
+        # only_find_one=True
+    )
+
+    graph, cond_sets_satisfying_cond_indep = skeleton_finder.find()
+
+    # we expect b-d in this intermediate stage. b-d is spurious, due to
+    # collider bias.
+
+    expected_undirected_edges = frozenset({
+        frozenset({'a', 'b'}),
+        frozenset({'b', 'd'}),
+        frozenset({'d', 'e'}),
+        frozenset({'b', 'c'}),
+        frozenset({'c', 'd'}),
+    })
+
+    assert frozenset(graph.undirected_edges) == expected_undirected_edges
+
+def test_chain_and_collider_without_MI(
+    df_chain_and_collider_without_MI
+):
+    size = 100000
+
+    df = df_chain_and_collider_without_MI(size=size)
+    skeleton_finder = SkeletonFinder(
+        data=df,
+        var_names=df.columns,
+        only_find_one=True
+    )
+
+    graph, cond_sets_satisfying_cond_indep = skeleton_finder.find()
+
+    expected_undirected_edges = frozenset({
+        frozenset({'a', 'b'}),
+        frozenset({'b', 'c'}),
+        frozenset({'a', 'd'}),
+        frozenset({'c', 'd'})
+    })
+
+    assert frozenset(graph.undirected_edges) == expected_undirected_edges
+
+def test_chain_and_collider_with_MI(
+    df_chain_and_collider_with_MI
+):
+    size = 100000
+
+    df = df_chain_and_collider_with_MI(size=size)
+    skeleton_finder = SkeletonFinder(
+        data=df,
+        var_names=df.columns,
+        only_find_one=True
+    )
+
+    graph, cond_sets_satisfying_cond_indep = skeleton_finder.find()
+
+    # we expect a-c in this intermediate stage. a-c is spurious, due to
+    # collider bias.
+
+    expected_undirected_edges = frozenset({
+        frozenset({'a', 'c'}),
+        frozenset({'a', 'b'}),
+        frozenset({'b', 'c'}),
+        frozenset({'a', 'd'}),
+        frozenset({'c', 'd'})
+    })
+
+    assert frozenset(graph.undirected_edges) == expected_undirected_edges
