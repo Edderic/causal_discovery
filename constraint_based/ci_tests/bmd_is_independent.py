ac97945cd9b9ba98f00e68a33f870cf11bb9378c
Edderic Ugaddan <eugaddan@panoramaed.com>
2021-01-22 11:30:57 -0500

    implement bmd_is_independent

---
+++ constraint_based/ci_tests/bmd_is_independent.py
@@ -0,0 +1,205 @@
+import numpy as np
+import pandas as pd
+
+def bmd_is_independent(
+    data,
+    vars_1=[],
+    vars_2=[],
+    conditioning_set=[],
+    threshold=1
+):
+    # If conditioning set has at least one item,
+    # vars_1 _||_ vars_2 | conditioning_set
+
+    # See if P(vars_1 | conditioning_set) = P(vars_1 | conditioning_set, vars_2)
+    var_1 = vars_1[0]
+    var_2 = vars_2[0]
+    _data = data.copy()
+    _data['tmp_count'] = 0
+
+    classes_for_var_1 = _data\
+        .groupby(var_1).count().index
+
+    classes_for_var_2 = _data\
+        .groupby(var_2).count().index
+
+    if conditioning_set == []:
+        p1 = posterior(
+            _data,
+            variable=var_1
+        )
+
+        cond_set_var_2 = {}
+
+        for var_2_val in classes_for_var_2:
+            cond_set_var_2[var_2] = var_2_val
+            p2 = posterior(
+                _data,
+                variable=var_1,
+                conditioning_set=cond_set_var_2
+            )
+
+            if is_dependent(p1, p2, threshold):
+                return False
+
+    else:
+        classes_for_cond_set = _data\
+            .groupby(conditioning_set).count().index
+        cond_set_names = classes_for_cond_set.names
+
+        for cond_set_val in classes_for_cond_set:
+            cond_set_p1 = {}
+
+            if len(cond_set_names) == 1:
+                cond_set_p1[cond_set_names[0]] = cond_set_val
+            else:
+                for cond_set_name, cond_val in zip(cond_set_names, cond_set_val):
+                    cond_set_p1[cond_set_name] = cond_val
+
+            p1 = posterior(
+                _data,
+                variable=var_1,
+                conditioning_set=cond_set_p1
+            )
+
+            for var_2_val in classes_for_var_2:
+                # make a copy
+                cond_set_p2 = dict(cond_set_p1)
+                cond_set_p2[var_2] = var_2_val
+
+                p2 = posterior(
+                    _data,
+                    variable=var_1,
+                    conditioning_set=cond_set_p2
+
+                )
+
+                if is_dependent(p1, p2, threshold):
+                    return False
+
+    return True
+
+def posterior(data, variable, conditioning_set={}, size=1000):
+    """
+        Parameters:
+            data: pd.DataFrame
+            variable: name of the variable
+            conditioning_set: dict
+                key: name of the variable
+                value: value of said variable
+
+    """
+
+    if conditioning_set == {}:
+        _counts = data.groupby(variable).count()
+        bdeu_prior = np.ones(_counts.shape[0]) / _counts.shape[0]
+
+        return np.random.dirichlet(
+            tuple((bdeu_prior + _counts['tmp_count'])),
+            size=size
+        )
+
+    data_copy = data.copy()
+
+    var_num_classes = data.groupby(variable).count().shape[0]
+
+    for key, val in conditioning_set.items():
+        data_copy = data_copy[data_copy[key] == val]
+
+    # TODO: what if it's deterministic?
+    counts = data_copy.groupby(variable).count()
+
+    bdeu_prior = np.ones(var_num_classes) / var_num_classes
+
+    data_count = make_data_counts_same_size_as_num_classes(
+        expected_num_classes=var_num_classes,
+        counts=counts
+    )
+
+    return np.random.dirichlet(
+        tuple((bdeu_prior + data_count)),
+        size=size
+    )
+
+def is_dependent(p1, p2, threshold):
+    acceptable_1 = (p1 > p2).sum(axis=0) / p1.shape[0] >= threshold
+    acceptable_2 = (p2 > p1).sum(axis=0) / p1.shape[0] >= threshold
+
+    # import pdb; pdb.set_trace()
+    # import matplotlib.pyplot as plt
+#
+    # fig = plt.figure()
+    # ax_1 = fig.add_subplot(111)
+    # ax_2 = fig.add_subplot(211)
+    # ax_3 = fig.add_subplot(311)
+#
+    # pd.DataFrame(p1).plot.hist(bins=100,alpha=0.5, ax=ax_1)
+    # pd.DataFrame(p2).plot.hist(bins=100,alpha=0.5, ax=ax_2)
+    # pd.DataFrame(p1 / p2).plot.hist(bins=100,alpha=0.5, ax=ax_3)
+#
+    # fig.savefig('test1.png')
+
+
+    return (acceptable_1 + acceptable_2).sum() > 0
+
+def make_data_counts_same_size_as_num_classes(expected_num_classes, counts):
+    """
+        When the number of classes in the counts is less than the expected
+        num_classes, add to it.
+    """
+    data_count = []
+
+    if counts['tmp_count'].shape[0] != expected_num_classes:
+        data_count = list(counts['tmp_count'].values)
+
+        while len(data_count) < expected_num_classes:
+            data_count.append(0)
+
+        return np.array(data_count)
+    else:
+        return counts['tmp_count']
+
+
+    # for index, data_count in _counts.iterrows():
+
+    # we get the groupby counts. Each row has coordinates that exist in the data
+    # However, there might be coordinates that don't exist in the data. We are
+    # also interested in those especially for the variable of interest.
+
+    # To get a posterior count, we need the number of possible values per
+    # variable in the data set. E.g. If there's a variable called
+    # "dominant-hand", the possible values are left-hand and right-hand, so the
+    # number of possible values for this variable is 2.
+
+    # To determine the Bdeu prior, we would need to know how many variables are
+    # in the conditioning set, and what the number of possible values are for
+    # each of those variables. Let's say in our data set, we have the variables
+    #   "dominant-hand"
+    #     - left-handed, right-handed,
+    #   "gender"
+    #     - trans, cis
+    #   "religion"
+    #     - catholic, protestant, muslim, hindu
+    #
+    # Let's say we're interested in "dominant-hand | gender, religion". There
+    # should be 2 Dirichlet priors for each combination of gender and
+    # religion. There are 2 x 4 = 8 combinations of gender and religion, so we
+    # make, 8 x 2 = 16 Dirichlet priors
+
+    # _counts = _data.groupby(list(set(variables).union(conditioning_set))).count()
+#
+    # posterior_samples = []
+#
+    # for index, data_count in _counts.iterrows():
+        # bdeu_prior = np.ones(num_classes_variable) / num_classes_variable
+#
+        # posterior_samples.append(np.random.dirichlet(bdeu_prior + data_count, size=size))
+#
+    # _counts['posterior_samples'] = posterior_samples
+#
+    # return _counts
+
+
+class Posterior(object):
+    def __init__(self, indices, posteriors):
+        pass
---
+++ constraint_based/ci_tests/bmd_is_independent_test.py
@@ -0,0 +1,146 @@
+import numpy as np
+import pandas as pd
+import pytest
+from .bmd_is_independent import bmd_is_independent, posterior
+
+def test_uniform_multinomial_with_4_possible_values_size_10000(
+    df_2_multinomial_indep_RVs
+):
+    params = {
+       "data": df_2_multinomial_indep_RVs(size=10000),
+       "vars_1": ['x'],
+       "vars_2": ['y'],
+       "conditioning_set": []
+    }
+
+    params["data"] = df_2_multinomial_indep_RVs(size=10000)
+    assert bmd_is_independent(**params) == True
+
+    params["data"] = df_2_multinomial_indep_RVs(size=1000)
+    assert bmd_is_independent(**params) == True
+
+    params["data"] = df_2_multinomial_indep_RVs(size=100)
+    assert bmd_is_independent(**params) == True
+
+def test_3_indep_small_100():
+    x = np.random.binomial(n=1, p=0.5, size=100)
+    y = np.random.binomial(n=1, p=0.5, size=100)
+    z = np.random.binomial(n=1, p=0.5, size=100)
+
+    df = pd.DataFrame({'x': x, 'y': y, 'z': z})
+
+    is_independent = bmd_is_independent(
+        data=df,
+        vars_1=['x'],
+        vars_2=['y'],
+        conditioning_set=['z']
+    )
+
+    assert is_independent == True
+
+def test_long_chains_collider_bias_without_MI(
+    df_long_chains_and_collider_without_MI
+):
+    df = df_long_chains_and_collider_without_MI(size=10000)
+
+    indep = bmd_is_independent(
+        data=df,
+        vars_1=['d'],
+        vars_2=['b']
+    )
+
+    assert indep == True
+
+    indep = bmd_is_independent(
+        data=df,
+        vars_1=['b'],
+        vars_2=['d']
+    )
+
+    assert indep == True
+
+def test_X_and_Y_are_deterministic(
+    df_X_and_Y_are_deterministic
+):
+    params = {
+       "data": df_X_and_Y_are_deterministic(size=100),
+       "vars_1": ['x'],
+       "vars_2": ['y'],
+       "conditioning_set": []
+    }
+
+    assert bmd_is_independent(**params) == False
+
+def test_X_causes_Y(
+    df_X_causes_Y
+):
+    params = {
+       "data": df_X_causes_Y(size=100),
+       "vars_1": ['x'],
+       "vars_2": ['y'],
+       "conditioning_set": []
+    }
+
+    assert bmd_is_independent(**params) == False
+
+def test_X_and_Y_cause_Z(
+    df_X_and_Y_cause_Z
+):
+    params_1 = {
+       "data": df_X_and_Y_cause_Z(size=10000),
+       "vars_1": ['x'],
+       "vars_2": ['y'],
+       "conditioning_set": []
+    }
+
+    assert bmd_is_independent(**params_1) == True
+
+    params_2 = {
+       "data": df_X_and_Y_cause_Z(size=10000),
+       "vars_1": ['x'],
+       "vars_2": ['y'],
+       "conditioning_set": ['z']
+    }
+
+    assert bmd_is_independent(**params_2) == False
+
+def test_Z_causes_X_and_Y(
+    df_Z_causes_X_and_Y
+):
+    params_1 = {
+       "data": df_Z_causes_X_and_Y(size=500),
+       "vars_1": ['x'],
+       "vars_2": ['y'],
+       "conditioning_set": ['z']
+    }
+
+    assert bmd_is_independent(**params_1) == True
+
+    params_2 = {
+       "data": df_Z_causes_X_and_Y(size=500),
+       "vars_1": ['x'],
+       "vars_2": ['y'],
+       "conditioning_set": []
+    }
+
+    assert bmd_is_independent(**params_2) == False
+
+def test_spurious_edge(
+    df_long_chains_and_collider_with_MI
+):
+    size = 10000
+
+    df = df_long_chains_and_collider_with_MI(size=size)
+
+    params = {
+       "data": df,
+       "vars_1": ['b'],
+       "vars_2": ['d'],
+       "conditioning_set": []
+    }
+
+    # we expect b-d to be a spurious edge because we're implicitly conditioning
+    # on the collider MI_b
+
+    assert bmd_is_independent(**params) == False
+
