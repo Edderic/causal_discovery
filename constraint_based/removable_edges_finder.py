853ef80e0f6527b08d22390f28773df22d65bbd0
Edderic Ugaddan <eugaddan@panoramaed.com>
2021-01-23 07:57:15 -0500

    add tests, use bmd_is_independent for RemovableEdgesFinder

--- constraint_based/removable_edges_finder.py
+++ constraint_based/removable_edges_finder.py
@@ -1,5 +1,5 @@
 from .density_ratio_weighted_correction import DensityRatioWeightedCorrection
-from .ci_tests.sci_is_independent import sci_is_independent
+from .ci_tests.bmd_is_independent import bmd_is_independent
 from .density_ratio_weighted_correction import DensityRatioWeightedCorrection
 from .misc import conditioning_sets_satisfying_conditional_independence
 
@@ -14,7 +14,7 @@ class RemovableEdgesFinder(object):
                 simulated data that adjusts for missingness. "correct" invocation
                 should return a pandas.DataFrame.
             is_conditionally_independent_func: function.
-                Defaults to constraint_based.ci_tests.sci_is_independent
+                Defaults to constraint_based.ci_tests.bmd_is_independent
 
                 Some function that tells us whether or not sets of variables
                 are independent from each other given a conditioning set.
@@ -24,20 +24,16 @@ class RemovableEdgesFinder(object):
         data,
         marked_pattern_graph,
         data_correction=DensityRatioWeightedCorrection,
-        is_conditionally_independent_func=sci_is_independent,
+        is_conditionally_independent_func=bmd_is_independent,
         potentially_extraneous_edges=[],
         missingness_indicator_prefix='MI_'
     ):
-        import pdb; pdb.set_trace()
-        self.data = data.merge(
-            data.isnull().add_prefix(missingness_indicator_prefix),
-            left_index=True,
-            right_index=True
-        )
+        self.data = data
         self.potentially_extraneous_edges = potentially_extraneous_edges
         self.marked_pattern_graph = marked_pattern_graph
         self.data_correction = data_correction
         self.is_conditionally_independent_func = is_conditionally_independent_func
+        self.missingness_indicator_prefix = missingness_indicator_prefix
 
     def find(self):
         if len(self.potentially_extraneous_edges) == 0:
@@ -45,7 +41,6 @@ class RemovableEdgesFinder(object):
 
         extraneous_edges = []
 
-        import pdb; pdb.set_trace()
         for potentially_extraneous_edge in self.potentially_extraneous_edges:
             var_name_1, var_name_2 = tuple(potentially_extraneous_edge)
 
@@ -54,7 +49,9 @@ class RemovableEdgesFinder(object):
                 var_name_1,
                 var_name_2,
                 is_conditionally_independent_func=self.is_conditionally_independent_func,
-                possible_conditioning_set_vars=[],
+                possible_conditioning_set_vars=self._possible_conditioning_set_vars(
+                    var_name_1, var_name_2
+                ),
                 indegree=8,
                 only_find_one=True,
                 data_correction=self.data_correction,
@@ -65,3 +62,11 @@ class RemovableEdgesFinder(object):
                 extraneous_edges.append(potentially_extraneous_edge)
 
         return extraneous_edges
+
+    def _missingness_indicators(self):
+        return self.data.columns[self.data.columns.str.contains(self.missingness_indicator_prefix)]
+
+    def _possible_conditioning_set_vars(self, var_name_1, var_name_2):
+        return (
+            set(self.data.columns) - set(self._missingness_indicators())
+        ) - set({var_name_1, var_name_2})
--- constraint_based/removable_edges_finder_test.py
+++ constraint_based/removable_edges_finder_test.py
@@ -4,7 +4,7 @@ from ..graphs.marked_pattern_graph import MarkedPatternGraph
 from .density_ratio_weighted_correction import DensityRatioWeightedCorrection
 
 def test_cond_on_collider(df_X_and_Y_cause_Z_and_Z_cause_MI_X):
-    df = df_X_and_Y_cause_Z_and_Z_cause_MI_X()
+    df = df_X_and_Y_cause_Z_and_Z_cause_MI_X(size=1000)
 
     # extraneous edge x-y
     graph = MarkedPatternGraph(
@@ -13,13 +13,117 @@ def test_cond_on_collider(df_X_and_Y_cause_Z_and_Z_cause_MI_X):
         marked_arrows=[('z', 'MI_x')]
     )
 
-    validator = RemovableEdgesFinder(
+    finder = RemovableEdgesFinder(
         data=df,
         marked_pattern_graph=graph,
         potentially_extraneous_edges=[set({'x', 'y'})],
         data_correction=DensityRatioWeightedCorrection,
     )
 
-    removables = validator.find()
+    removables = finder.find()
 
     assert removables == [set({'x', 'y'})]
+
+def test_long_chains_and_collider_with_MI(df_long_chains_and_collider_with_MI):
+    df = df_long_chains_and_collider_with_MI(size=1000, proba_noise=0.6)
+
+    graph = MarkedPatternGraph(
+        nodes=list(set(df.columns).union(set({'MI_b'}))),
+        undirected_edges=set({
+            frozenset({'b', 'a'}),
+            frozenset({'d', 'e'}),
+            frozenset({'d', 'c'}),
+            frozenset({'b', 'c'}),
+            frozenset({'d', 'b'})
+        }),
+        marked_arrows=[('c', 'MI_b')]
+    )
+
+    finder = RemovableEdgesFinder(
+        data=df,
+        marked_pattern_graph=graph,
+        potentially_extraneous_edges=set({
+            frozenset({'d', 'b'}),
+            frozenset({'d', 'c'}),
+            frozenset({'b', 'c'})
+        }),
+        data_correction=DensityRatioWeightedCorrection,
+    )
+
+    removables = finder.find()
+
+    assert set(removables) == set({ frozenset({'b', 'd'}) })
+
+def test_3_multinom_RVs_MAR(
+    df_Z_causes_X_Y_and_X_Z_causes_MI_Y
+):
+    size = 1000
+
+    df = df_Z_causes_X_Y_and_X_Z_causes_MI_Y(size=size)
+
+    graph = MarkedPatternGraph(
+        nodes=list(set(df.columns).union(set({'MI_y'}))),
+        undirected_edges=set({
+            frozenset({'x', 'y'}), # extraneous edge
+            frozenset({'x', 'z'}),
+            frozenset({'z', 'y'}),
+        }),
+        marked_arrows=[
+            ('x', 'MI_y'),
+            ('z', 'MI_y')
+        ]
+    )
+
+    finder = RemovableEdgesFinder(
+        data=df,
+        marked_pattern_graph=graph,
+        potentially_extraneous_edges=set({
+            frozenset({'x', 'y'}),
+        }),
+        data_correction=DensityRatioWeightedCorrection,
+    )
+
+    removables = finder.find()
+
+    assert set(removables) == set({ frozenset({'x', 'y'}) })
+
+def test_chain_and_collider_with_MI(
+    df_chain_and_collider_with_MI
+):
+    size = 10000
+
+    df = df_chain_and_collider_with_MI(size=size)
+
+    graph = MarkedPatternGraph(
+        nodes=list(set(df.columns).union(set({'MI_y'}))),
+        undirected_edges=set({
+            frozenset({'a', 'c'}), # extraneous edge
+            frozenset({'a', 'b'}),
+            frozenset({'b', 'c'}),
+            frozenset({'a', 'd'}),
+            frozenset({'c', 'd'}),
+        }),
+        marked_arrows=[
+            ('d', 'MI_a')
+        ]
+    )
+
+    # we expect a-c in this intermediate stage. a-c is spurious, due to
+    # collider bias.
+
+    expected_undirected_edges = frozenset({
+        frozenset({'a', 'c'}),
+    })
+
+    finder = RemovableEdgesFinder(
+        data=df,
+        marked_pattern_graph=graph,
+        potentially_extraneous_edges=set({
+            frozenset({'a', 'c'}),
+        }),
+        data_correction=DensityRatioWeightedCorrection,
+    )
+
+    removables = finder.find()
+
+    assert set(removables) == set({ frozenset({'a', 'c'}) })
