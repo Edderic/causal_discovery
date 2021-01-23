ad7a0c7f03ab3879b0196b4ba7c674005b32a970
Edderic Ugaddan <eugaddan@panoramaed.com>
2021-01-23 07:56:16 -0500

    fix, clean DirectCausesOfMissingnessFinder

--- constraint_based/direct_causes_of_missingness_finder_test.py
+++ constraint_based/direct_causes_of_missingness_finder_test.py
@@ -2,49 +2,34 @@ import numpy as np
 import pytest
 from .skeleton_finder import SkeletonFinder
 from .direct_causes_of_missingness_finder import DirectCausesOfMissingnessFinder
+from ..graphs.marked_pattern_graph import MarkedPatternGraph
 
 def test_2_multinom_RVs_MCAR(
     df_2_multinomial_indep_RVs
 ):
     size = 1000
     df = df_2_multinomial_indep_RVs(size=size)
+
     missingness_of_x = np.random.binomial(n=1, p=0.3, size=size)
     missingness_indices = np.where(missingness_of_x == 1)
 
     df.at[missingness_indices[0], 'x'] = np.nan
 
-    skeleton_finder = SkeletonFinder(
-        data=df,
-        var_names=df.columns,
-    )
-
-    graph, cond_sets_satisfying_cond_indep = \
-        skeleton_finder.find()
-
     direct_causes_of_missingness_finder = DirectCausesOfMissingnessFinder(
         data=df
     )
 
     marked_arrows = direct_causes_of_missingness_finder.find()
 
-    assert set(graph.nodes).intersection(set(['x', 'y', 'MI_x']))
-    assert graph.undirected_edges == []
+    assert marked_arrows == []
 
 def test_2_multinom_RVs_MAR(
     df_X_Y_indep_Y_causes_MI_X
 ):
 
-    size = 1000
+    size = 2000
 
     df = df_X_Y_indep_Y_causes_MI_X(size=size)
-
-    skeleton_finder = SkeletonFinder(
-        data=df,
-        var_names=df.columns,
-    )
-
-    graph, cond_sets_satisfying_cond_indep = \
-        skeleton_finder.find()
 
     direct_causes_of_missingness_finder = DirectCausesOfMissingnessFinder(
         data=df
@@ -52,8 +37,6 @@ def test_2_multinom_RVs_MAR(
 
     marked_arrows = direct_causes_of_missingness_finder.find()
 
-    assert set(graph.nodes).intersection(set(['x', 'y', 'MI_x']))
-    assert graph.undirected_edges == []
     assert marked_arrows == [('y', 'MI_x')]
 
 def test_3_multinom_RVs_MAR(
@@ -63,21 +46,11 @@ def test_3_multinom_RVs_MAR(
 
     df = df_Z_causes_X_Y_and_X_Z_causes_MI_Y(size=size)
 
-    skeleton_finder = SkeletonFinder(
-        data=df,
-        var_names=df.columns,
-    )
-
-    graph, cond_sets_satisfying_cond_indep = \
-        skeleton_finder.find()
-
     direct_causes_of_missingness_finder = DirectCausesOfMissingnessFinder(
         data=df
     )
 
     marked_arrows = direct_causes_of_missingness_finder.find()
-    assert set(graph.nodes).intersection(set(['x', 'y', 'MI_x']))
-    assert graph.undirected_edges == [set(('x', 'z')), set(('z', 'y'))]
     assert set(marked_arrows) == set([('z', 'MI_y'), ('x', 'MI_y')])
 
 def test_long_chains_collider_bias_without_MI(
@@ -86,24 +59,6 @@ def test_long_chains_collider_bias_without_MI(
     size = 1000
 
     df = df_long_chains_and_collider_without_MI(size=size)
-    skeleton_finder = SkeletonFinder(
-        data=df,
-        var_names=df.columns,
-    )
-
-    graph, cond_sets_satisfying_cond_indep = skeleton_finder.find()
-
-    # we expect b-d in this intermediate stage. b-d is spurious, due to
-    # collider bias.
-
-    expected_undirected_edges = frozenset({
-        frozenset({'a', 'b'}),
-        frozenset({'d', 'e'}),
-        frozenset({'b', 'c'}),
-        frozenset({'c', 'd'}),
-    })
-
-    assert frozenset(graph.undirected_edges) == expected_undirected_edges
 
     direct_causes_of_missingness_finder = DirectCausesOfMissingnessFinder(
         data=df
@@ -115,32 +70,12 @@ def test_long_chains_collider_bias_without_MI(
 
     assert frozenset(marked_arrows) == expected_marked_arrows
 
-def test_long_chains_collider_bias_with_MI(
-    df_long_chains_and_collider_with_MI
+def test_chain_and_collider_with_MI(
+    df_chain_and_collider_with_MI
 ):
     size = 10000
 
-    df = df_long_chains_and_collider_with_MI(size=size)
-    skeleton_finder = SkeletonFinder(
-        data=df,
-        var_names=df.columns,
-        only_find_one=True
-    )
-
-    graph, cond_sets_satisfying_cond_indep = skeleton_finder.find()
-
-    # we expect b-d in this intermediate stage. b-d is spurious, due to
-    # collider bias.
-
-    expected_undirected_edges = frozenset({
-        frozenset({'a', 'b'}),
-        frozenset({'b', 'd'}),
-        frozenset({'d', 'e'}),
-        frozenset({'b', 'c'}),
-        frozenset({'c', 'd'}),
-    })
-
-    assert frozenset(graph.undirected_edges) == expected_undirected_edges
+    df = df_chain_and_collider_with_MI(size=size)
 
     direct_causes_of_missingness_finder = DirectCausesOfMissingnessFinder(
         data=df
@@ -149,7 +84,26 @@ def test_long_chains_collider_bias_with_MI(
     marked_arrows = direct_causes_of_missingness_finder.find()
 
     expected_marked_arrows = frozenset({
-      ('c', 'MI_a')
+      ('d', 'MI_a')
+    })
+
+    assert frozenset(marked_arrows) == expected_marked_arrows
+
+def test_long_chains_collider_bias_with_MI(
+    df_long_chains_and_collider_with_MI
+):
+    size = 1000
+
+    df = df_long_chains_and_collider_with_MI(size=size)
+
+    direct_causes_of_missingness_finder = DirectCausesOfMissingnessFinder(
+        data=df
+    )
+
+    marked_arrows = direct_causes_of_missingness_finder.find()
+
+    expected_marked_arrows = frozenset({
+      ('c', 'MI_b')
     })
 
     assert frozenset(marked_arrows) == expected_marked_arrows
