1466d8306f826c3e5146c4eacc46eec42b4784ed
Edderic Ugaddan <eugaddan@panoramaed.com>
2021-01-25 09:59:38 -0500

    increase sample size to decrease test brittleness

--- constraint_based/removable_edges_finder_test.py
+++ constraint_based/removable_edges_finder_test.py
@@ -4,7 +4,7 @@ from ..graphs.marked_pattern_graph import MarkedPatternGraph
 from .density_ratio_weighted_correction import DensityRatioWeightedCorrection
 
 def test_cond_on_collider(df_X_and_Y_cause_Z_and_Z_cause_MI_X):
-    df = df_X_and_Y_cause_Z_and_Z_cause_MI_X(size=1000)
+    df = df_X_and_Y_cause_Z_and_Z_cause_MI_X(size=2000)
 
     # extraneous edge x-y
     graph = MarkedPatternGraph(
