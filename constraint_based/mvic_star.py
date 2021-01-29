c70fce581d993a003c1b6df5ddc512eb0cb9b747
Edderic Ugaddan <eugaddan@panoramaed.com>
2021-01-29 10:32:10 -0500

    add debugging

--- constraint_based/mvic_star.py
+++ constraint_based/mvic_star.py
@@ -29,7 +29,9 @@ class MVICStar(object):
         self.orig_columns = data.columns
         self.missingness_indicator_prefix = missingness_indicator_prefix
 
-    def predict(self):
+    def predict(self, debug=False):
+        self.debug_info = []
+
         skeleton_finder = SkeletonFinder(
             var_names=self.orig_columns,
             data=self.data
@@ -37,15 +39,26 @@ class MVICStar(object):
 
         graph, cond_sets_satisfying_cond_indep = skeleton_finder.find()
 
+        self.debug_info.append({
+            'name': 'after skeleton finding',
+            'graph': graph.copy(),
+            'cond_sets_satisfying_cond_indep': cond_sets_satisfying_cond_indep
+        })
+
         marked_arrows = DirectCausesOfMissingnessFinder(
-            data=self.data
+            data=self.data,
+            missingness_indicator_prefix=self.missingness_indicator_prefix
         ).find()
 
         graph.add_marked_arrows(marked_arrows)
 
+        self.debug_info.append({
+            'name': 'after adding direct causes of missingness',
+            'graph': graph.copy()
+        })
+
         potentially_extraneous_edges = PotentiallyExtraneousEdgesFinder(
             marked_pattern_graph=graph,
-            missingness_indicator_prefix=self.missingness_indicator_prefix
         ).find()
 
         edges_to_remove = RemovableEdgesFinder(
@@ -53,10 +66,16 @@ class MVICStar(object):
             data_correction=DensityRatioWeightedCorrection,
             potentially_extraneous_edges=potentially_extraneous_edges,
             cond_sets_satisfying_cond_indep=cond_sets_satisfying_cond_indep,
-            marked_pattern_graph=graph
+            marked_pattern_graph=graph,
+            missingness_indicator_prefix=self.missingness_indicator_prefix
         ).find()
 
         graph.remove_undirected_edges(edges_to_remove)
+
+        self.debug_info.append({
+            'name': 'after removing undirected edges',
+            'graph': graph.copy()
+        })
 
         immoralities = ImmoralitiesFinder(
             marked_pattern_graph=graph,
@@ -65,7 +84,17 @@ class MVICStar(object):
 
         graph.add_arrowheads(immoralities)
 
+        self.debug_info.append({
+            'name': 'after adding immoralities',
+            'graph': graph.copy()
+        })
+
         RecursiveEdgeOrienter(marked_pattern_graph=graph).orient()
+
+        self.debug_info.append({
+            'name': 'after recursively orienting edges',
+            'graph': graph.copy()
+        })
 
         return graph
 
