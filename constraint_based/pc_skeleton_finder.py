fdaea361bb105d03225bbeb786c4f9703ee725ab
Edderic Ugaddan <eugaddan@panoramaed.com>
2021-02-07 18:43:19 -0500

    add pc_skeleton_finder with dual pass

---
+++ constraint_based/pc_skeleton_finder.py
@@ -0,0 +1,202 @@
+from constraint_based.ci_tests.bmd_is_independent import bmd_is_independent
+from constraint_based.ci_tests.sci_is_independent import sci_is_independent
+from itertools import combinations
+from constraint_based.misc import conditioning_sets_satisfying_conditional_independence, key_for_pair
+from graphs.marked_pattern_graph import MarkedPatternGraph
+from tqdm import tqdm
+from constraint_based.misc import setup_logging, ConditioningSets
+
+# TODO: rename since this is inspired
+class PCSkeletonFinder():
+    """
+        Finds the set of undirected edges among nodes.
+
+        Parameters:
+            var_names: [list[str]]
+                The names of variables that will be in the Skeleton. e.g.
+                ["Race", "Weight"]
+
+            data [pandas.DataFrame]
+                Contains data. Each column is a variable. Each column name must
+                match one and only of the var_names.
+    """
+    def __init__(
+        self,
+        var_names,
+        data,
+        is_conditionally_independent_func=bmd_is_independent,
+        missing_indicator_prefix='MI_',
+        only_find_one=False
+    ):
+        self.var_names = var_names
+
+        self.data = data
+        self.orig_cols = list(data.columns)
+        self.is_conditionally_independent_func = is_conditionally_independent_func
+        self.missing_indicator_prefix = missing_indicator_prefix
+        self.only_find_one = only_find_one
+
+    def find(self):
+        """
+            Go through each pair of variables (in var_names).
+            For each pair, find a conditioning set that renders the two variables
+            independent.
+
+            Returns:
+                marked_pattern: MarkedPatternGraph
+                    It'll store the skeleton (a set of undirected edges). It
+                    can be used for later steps, such as finding immoralities.
+
+                cond_sets: dict
+
+                    key: str.
+                        The pair of variables that are conditionally
+                        independent, delimited by " _||_ ".  E.g. If "X _||_ Y"
+                        is a key, then X and Y are the variables that are
+                        conditionally independent.
+
+                    value: list(sets(str)).
+                        The conditioning sets that make X and Y conditionally
+                        independent.
+        """
+        undirected_edges = []
+        self.cond_sets = ConditioningSets()
+
+        self.graph = self._init_complete_graph()
+
+        # make the graph as sparse as it can.
+        self._skeleton_find()
+        # update conditioning sets that we missed (to assist with helping find immoralities)
+        self._find_more_independencies()
+
+        return self.graph, self.cond_sets
+
+    def _skeleton_find(self):
+        depth = 0
+
+        logging = setup_logging()
+
+        while self._depth_not_greater_than_num_adj_nodes_per_var(depth):
+            logging.info("Finding skeleton. Depth: {}".format(depth))
+
+            for undirected_edge in self.graph.get_undirected_edges():
+
+                node_1, node_2 = tuple(undirected_edge)
+
+                for ordered_edge in [(node_1, node_2), (node_2, node_1)]:
+                    ordered_node_1, ordered_node_2 = ordered_edge
+
+                    conditionables = list(
+                            self.graph.get_neighbors(ordered_node_1)\
+                             - set({ordered_node_2})
+                    )
+
+                    if len(conditionables) >= depth:
+                        for combo in combinations(conditionables, depth):
+                            if self.is_conditionally_independent_func(
+                                self.data,
+                                vars_1=[ordered_node_1],
+                                vars_2=[ordered_node_2],
+                                conditioning_set=list(combo)
+                            ):
+
+                                self.graph.remove_undirected_edge(
+                                    (node_1, node_2)
+                                )
+
+                                self.cond_sets.add(node_1, node_2, combo)
+
+                                break
+
+            depth += 1
+
+    def _find_more_independencies(self):
+        depth = 0
+
+        logging = setup_logging()
+
+        while self._depth_not_greater_than_num_adj_nodes_per_var(depth):
+            logging.info("Finding more independencies. Depth: {}".format(depth))
+
+            nodes = self.graph.get_nodes()
+
+            for node_1, node_2 in combinations(nodes, 2):
+                node_1_neighbors = self.graph.get_neighbors(node_1)
+                node_2_neighbors = self.graph.get_neighbors(node_2)
+
+                # if no neighbors, then there exists no path to node_2, so
+                # independent by default
+                if len(node_1_neighbors) == 0 or len(node_2_neighbors) == 0:
+                    continue
+
+                # if node_1 and node_2 are neighbors, then there is no
+                # conditioning set that separates them, so move on.
+                if node_1_neighbors.intersection(set({node_2})) == set({node_2}):
+                    continue
+
+                conditionables = list(node_1_neighbors.union(node_2_neighbors) - set({node_1, node_2}))
+
+                for conditionable in combinations(conditionables, depth):
+                    if self.is_conditionally_independent_func(
+                        self.data,
+                        vars_1=[node_1],
+                        vars_2=[node_2],
+                        conditioning_set=list(conditionable)
+                    ):
+                        self.cond_sets.add(node_1, node_2, conditionable)
+
+            depth += 1
+
+
+    def _init_complete_graph(self):
+        return MarkedPatternGraph(
+            nodes=list(self.data.columns),
+            undirected_edges=list(combinations(self.orig_cols, 2))
+        )
+
+    def _depth_not_greater_than_num_adj_nodes_per_var(self, depth):
+        undirected_edges = self.graph.get_undirected_edges()
+
+        if len(undirected_edges) == 0:
+            return False
+
+        for undirected_edge in undirected_edges:
+            ordered_edge_1 = tuple(undirected_edge)
+            ordered_edge_2 = (ordered_edge_1[1], ordered_edge_1[0])
+
+            for ordered_edge in [ordered_edge_1, ordered_edge_2]:
+                node_1, node_2 = tuple(ordered_edge)
+
+                node_1_neighbors_except_node_2 = list(self.graph.get_neighbors(node_1) - set({node_2}))
+
+                if len(node_1_neighbors_except_node_2) >= depth:
+                    return True
+
+        return False
+
+    def _is_independent(self, var_name_1, var_name_2):
+        """
+            Goes through possible conditioning sets, including the empty set.
+        """
+
+        for cond_set_length in np.arange(len(self.var_names) - 1):
+            cond_set_combos = combinations(
+                    list(
+                        set(self.var_names) - set([var_name_1, var_name_2])
+                        ),
+                    cond_set_length
+                    )
+
+
+            for cond_set_combo in cond_set_combos:
+                if self.is_conditionally_independent_func(
+                        data=self.data,
+                        vars_1=[var_name_1],
+                        vars_2=[var_name_2],
+                        conditioning_set=list(cond_set_combo),
+                        ):
+
+                    return True
+
+        return False
+
---
+++ constraint_based/pc_skeleton_finder_test.py
@@ -0,0 +1,254 @@
+import pytest
+import numpy as np
+import pandas as pd
+from constraint_based.pc_skeleton_finder import PCSkeletonFinder
+from constraint_based.ci_tests.sci_is_independent import sci_is_independent
+from data import dog_example
+from constraint_based.misc import key_for_pair
+
+# PCSkeletonFinder is missing an edge because the distribution we used is
+# unstable. It has independencies that is incompatible with the true DAG
+def test_2_deterministic_and_3rd_var_caused_by_one_of_them(
+    df_2_deterministic_and_3rd_var_caused_by_one_of_them
+):
+    skeleton_finder = PCSkeletonFinder(
+        data=df_2_deterministic_and_3rd_var_caused_by_one_of_them(size=1000),
+        var_names=['x', 'y', 'z'],
+    )
+
+    graph, cond_sets_satisfying_cond_indep = skeleton_finder.find()
+
+    assert graph.get_undirected_edges() == set({frozenset({'x', 'y'})})
+    assert graph.get_nodes() == set({'x', 'y', 'z'})
+
+def test_2_multinom_RVs(df_2_multinomial_indep_RVs):
+    skeleton_finder = PCSkeletonFinder(
+        data=df_2_multinomial_indep_RVs(size=10000),
+        var_names=['x', 'y'],
+    )
+
+    graph, cond_sets_satisfying_cond_indep = skeleton_finder.find()
+
+    assert graph.get_undirected_edges() == set({})
+    assert cond_sets_satisfying_cond_indep['x _||_ y'] == set({frozenset({})})
+
+def test_skeleton_finder_X_causes_Y(df_X_causes_Y):
+    skeleton_finder = PCSkeletonFinder(
+        data=df_X_causes_Y(size=1000),
+        var_names=['x', 'y'],
+    )
+
+    graph, cond_sets_satisfying_cond_indep = skeleton_finder.find()
+
+    assert graph.get_undirected_edges() == set({frozenset(('x', 'y'))})
+    assert cond_sets_satisfying_cond_indep == {}
+
+def test_skeleton_finder_Z_causes_X_and_Y(df_Z_causes_X_and_Y):
+    var_names = ['x', 'y', 'z']
+
+    skeleton_finder = PCSkeletonFinder(
+        data=df_Z_causes_X_and_Y(size=1000),
+        var_names=var_names,
+    )
+
+    graph, cond_sets_satisfying_cond_indep = skeleton_finder.find()
+
+    assert graph.nodes == var_names
+    assert graph.get_undirected_edges() == set({
+        frozenset(('x', 'z')),
+        frozenset(('y', 'z'))
+    })
+    assert cond_sets_satisfying_cond_indep == {'x _||_ y': set({frozenset({'z'})})}
+
+def test_long_chains_collider_bias_without_MI(
+    df_long_chains_and_collider_without_MI
+):
+    size = 100000
+
+    df = df_long_chains_and_collider_without_MI(size=size)
+    skeleton_finder = PCSkeletonFinder(
+        data=df,
+        var_names=df.columns,
+        only_find_one=True
+    )
+
+    graph, cond_sets_satisfying_cond_indep = skeleton_finder.find()
+
+    expected_undirected_edges = frozenset({
+        frozenset({'a', 'b'}),
+        frozenset({'d', 'e'}),
+        frozenset({'b', 'c'}),
+        frozenset({'c', 'd'}),
+    })
+
+    assert frozenset(graph.get_undirected_edges()) == expected_undirected_edges
+
+def test_long_chains_collider_bias_with_MI(
+    df_long_chains_and_collider_with_MI
+):
+    size = 10000
+
+    df = df_long_chains_and_collider_with_MI(size=size, proba_noise=0.7)
+    skeleton_finder = PCSkeletonFinder(
+        data=df,
+        var_names=df.columns,
+        # only_find_one=True
+    )
+
+    graph, cond_sets_satisfying_cond_indep = skeleton_finder.find()
+
+    # we expect b-d in this intermediate stage. b-d is spurious, due to
+    # collider bias.
+
+    expected_undirected_edges = frozenset({
+        frozenset({'a', 'b'}),
+        frozenset({'b', 'd'}),
+        frozenset({'d', 'e'}),
+        frozenset({'b', 'c'}),
+        frozenset({'c', 'd'}),
+    })
+
+    assert frozenset(graph.get_undirected_edges()) == expected_undirected_edges
+
+def test_chain_and_collider_without_MI(
+    df_chain_and_collider_without_MI
+):
+    size = 10000
+
+    df = df_chain_and_collider_without_MI(size=size)
+    skeleton_finder = PCSkeletonFinder(
+        data=df,
+        var_names=df.columns,
+        only_find_one=True
+    )
+
+    graph, cond_sets_satisfying_cond_indep = skeleton_finder.find()
+
+    expected_undirected_edges = frozenset({
+        frozenset({'a', 'b'}),
+        frozenset({'b', 'c'}),
+        frozenset({'a', 'd'}),
+        frozenset({'c', 'd'})
+    })
+
+    assert frozenset(graph.get_undirected_edges()) == expected_undirected_edges
+
+def test_chain_and_collider_with_MI(
+    df_chain_and_collider_with_MI
+):
+    size = 20000
+
+    df = df_chain_and_collider_with_MI(size=size)
+    skeleton_finder = PCSkeletonFinder(
+        data=df,
+        var_names=df.columns,
+        only_find_one=True
+    )
+
+    graph, cond_sets_satisfying_cond_indep = skeleton_finder.find()
+
+    # we expect a-c in this intermediate stage. a-c is spurious, due to
+    # collider bias.
+
+    expected_undirected_edges = frozenset({
+        frozenset({'a', 'c'}),
+        frozenset({'a', 'b'}),
+        frozenset({'b', 'c'}),
+        frozenset({'a', 'd'}),
+        frozenset({'c', 'd'})
+    })
+
+    assert frozenset(graph.get_undirected_edges()) == expected_undirected_edges
+
+def test_3_multinom_RVs_MAR(
+    df_Z_causes_X_Y_and_X_Z_causes_MI_Y
+):
+    size = 70000
+
+    df = df_Z_causes_X_Y_and_X_Z_causes_MI_Y(size=size)
+
+    skeleton_finder = PCSkeletonFinder(
+        data=df,
+        var_names=df.columns,
+    )
+
+    graph, cond_sets_satisfying_cond_indep = \
+        skeleton_finder.find()
+
+    assert set(graph.nodes).intersection(set(['x', 'y', 'MI_x']))
+    assert set(graph.get_undirected_edges()) == frozenset({frozenset(('x', 'z')), frozenset(('z', 'y'))})
+
+def test_dog_pee():
+    size = 100000
+
+    # Sometimes cloudy
+    cloudy = np.random.binomial(n=1, p=0.5, size=size)
+
+    # Cloudyness causes rain, but sometimes it rains even when it's not cloudy.
+    rain = cloudy * np.random.binomial(n=1, p=0.7, size=size) + (1 - cloudy) * np.random.binomial(n=1, p=0.1, size=size)
+
+    # Sprinkler generally turns on when it isn't cloudy.
+    sprinkler = (cloudy == 0) * np.random.binomial(n=1, p=0.8, size=size) + cloudy * np.random.binomial(n=1, p=0.1, size=size)
+
+    # Grass is generally wet whenever it rained or the sprinkler is on.
+    wet_grass = (rain | sprinkler) * np.random.binomial(n=1, p=0.90, size=size)
+
+    # Dog doesn't like to get rained on
+    # Dog goes out more frequently when it's not raining
+    dog_goes_out_to_pee = rain * np.random.binomial(n=1, p=0.2, size=size) + (1 - rain) * np.random.binomial(n=1, p=0.9, size=size)
+
+    df = pd.DataFrame({
+        'cloudy': cloudy,
+        'sprinkler': sprinkler,
+        'rain': rain,
+        'wet_grass': wet_grass,
+        'dog_goes_out_to_pee': dog_goes_out_to_pee
+    })
+
+    skeleton_finder = PCSkeletonFinder(
+        data=df,
+        var_names=df.columns,
+    )
+
+    graph, cond_sets_satisfying_cond_indep = \
+        skeleton_finder.find()
+
+    assert set(graph.get_undirected_edges()) == frozenset({
+        frozenset(('cloudy', 'rain')),
+        frozenset(('cloudy', 'sprinkler')),
+        frozenset(('rain', 'dog_goes_out_to_pee')),
+        frozenset(('rain', 'wet_grass')),
+        frozenset(('sprinkler', 'wet_grass')),
+    })
+
+def test_dog_example():
+    df = dog_example(size=100000)
+
+    skeleton_finder = PCSkeletonFinder(
+        data=df,
+        var_names=df.columns,
+    )
+
+    graph, cond_sets_satisfying_cond_indep = \
+        skeleton_finder.find()
+
+    assert cond_sets_satisfying_cond_indep[
+        key_for_pair(('activity', 'dog_tired'))
+    ].intersection(set({frozenset({'exercise_levels'})})) == set({frozenset({'exercise_levels'})})
+
+    assert cond_sets_satisfying_cond_indep[ key_for_pair(('weekend', 'mentally_exhausted_before_bed')) ]\
+            .intersection(set({frozenset({'best_friends_visit', 'activity'})})) \
+        == set({frozenset({'best_friends_visit', 'activity'})})
+
+    assert graph.get_undirected_edges() == frozenset({
+        frozenset(('rain', 'best_friends_visit')),
+        frozenset(('weekend', 'best_friends_visit')),
+        frozenset(('rain', 'activity')),
+        frozenset(('exercise_levels', 'best_friends_visit')),
+        frozenset(('exercise_levels', 'activity')),
+        frozenset(('mentally_exhausted_before_bed', 'activity')),
+        frozenset(('exercise_levels', 'dog_tired')),
+        frozenset(('best_friends_visit', 'mentally_exhausted_before_bed')),
+        frozenset(('mentally_exhausted_before_bed', 'carry_dog_for_last_potty_before_bed')),
+        frozenset(('dog_tired', 'carry_dog_for_last_potty_before_bed')),
+    })
